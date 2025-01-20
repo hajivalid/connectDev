d712b3b508081b9f88287c2de8d65e106a479f4f
hajivalid <hajivalid@gmail.com>
2025-01-21 00:54:37 +0530

    created a express server

---
+++ src/app.js
@@ -0,0 +1,15 @@
+const express = require('express');
+
+const app = express();
+
+app.use('/', (req, res)=>{
+    res.send('Hello from the Server Home Page!');
+})
+
+app.use('/test', (req, res)=>{
+    res.send('Hello from the Server!!');
+})
+
+app.listen(9993, ()=>{
+    console.log('Server is successfully listening on port 9993...');
+})
