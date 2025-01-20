0205027e430a7dfe9e75caa9ec842f13752d7adf
hajivalid <hajivalid@gmail.com>
2025-01-21 02:36:39 +0530

    explore routing

--- src/app.js
+++ src/app.js
@@ -2,24 +2,23 @@ const express = require('express');
 
 const app = express();
 
+//'/user/:userId'  --req.params
+//'/user/userId=101' -- req.query
+//'/user/abc' --normal
+//'/user/ab?c'  --b optional
+//'/user/ab+c'  --bbbbbbb... works fine
+//'/user/ab*cd'  -- anything between ab12345cd
+
 app.get('/user', (req, res) => {
+    //console.log(req.query);
     res.send({firstName:"Ehan", lastName:"Shami"});
 })
-
 app.post('/user', (req, res) => {
     res.send('successfully saved user data in DB');
 })
 
 app.delete('/user', (req, res) => {
     res.send('successfully deleted user data in DB');
-})
-
-app.use('/test', (req, res)=>{
-    res.send('Hello from the test Server!!');
-})
-
-app.use('/', (req, res)=>{
-    res.send('Hello from the Server Home Page!!');
 })
 
 app.listen(9993, ()=>{
