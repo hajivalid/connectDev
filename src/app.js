2f296de1c130fdec23f5c8647a0d9169fde486ba
hajivalid <hajivalid@gmail.com>
2025-01-21 02:11:07 +0530

    exploring get, post, delete

--- src/app.js
+++ src/app.js
@@ -2,12 +2,24 @@ const express = require('express');
 
 const app = express();
 
-app.use('/', (req, res)=>{
-    res.send('Hello from the Server Home Page!!');
+app.get('/user', (req, res) => {
+    res.send({firstName:"Ehan", lastName:"Shami"});
+})
+
+app.post('/user', (req, res) => {
+    res.send('successfully saved user data in DB');
+})
+
+app.delete('/user', (req, res) => {
+    res.send('successfully deleted user data in DB');
 })
 
 app.use('/test', (req, res)=>{
     res.send('Hello from the test Server!!');
+})
+
+app.use('/', (req, res)=>{
+    res.send('Hello from the Server Home Page!!');
 })
 
 app.listen(9993, ()=>{
