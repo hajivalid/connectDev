ba6c0a03ba93a7ee3d4259c5a4a6ced0c527f697
hajivalid <hajivalid@gmail.com>
2025-01-25 20:00:05 +0530

    Exploring validator validations

--- src/models/user.js
+++ src/models/user.js
@@ -1,4 +1,5 @@
 const mongoose = require("mongoose");
+const validator = require("validator");
 
 const userSchema = mongoose.Schema({
   firstName: {
@@ -18,11 +19,35 @@ const userSchema = mongoose.Schema({
     required: true,
     unique: true,
     lowercase: true,
-    trim: true
+    trim: true,
+    validate(val) {
+        if(!validator.isEmail(val)){
+            throw new Error("Email is not valid !!!")
+        }
+    }
   },
   password: {
     type: String,
     required: true,
+    validate(val) {
+        if(!validator.isStrongPassword(val)){
+            let match = {
+                minLength: 8,
+                minLowercase: 1,
+                minUppercase: 1,
+                minNumbers: 1,
+                minSymbols: 1,
+                returnScore: false,
+                pointsPerUnique: 1,
+                pointsPerRepeat: 0.5,
+                pointsForContainingLower: 10,
+                pointsForContainingUpper: 10,
+                pointsForContainingNumber: 10,
+                pointsForContainingSymbol: 10
+            };
+            throw new Error(`Password should match with: ${JSON.stringify(match)} but got value: ${val}`);
+        }
+    }
   },
   age: {
     type: Number,
@@ -33,18 +58,23 @@ const userSchema = mongoose.Schema({
     lowercase: true,
     validate(value) {
         if(!["male", "female", "others"].includes(value))
-            throw new Error("Gender data is not valid !!!")
+            throw new Error("Gender data is not valid: " + value)
     }
   },
   photoUrl: {
     type:String,
-    default: "https://cdn-icons-png.flaticon.com/512/9187/9187532.png"
+    default: "https://cdn-icons-png.flaticon.com/512/9187/9187532.png",
+    validate(val) {
+        if(!validator.isURL(val)){
+            throw new Error("Photo URL not valid: " + val)
+        }
+    }
   },
   phone: {
     type: Number,
     required: true,
     unique: true,
-    min: 10
+    min: 10,
   },
   skills: {
     type: [String],
